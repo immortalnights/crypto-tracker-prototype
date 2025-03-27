be0583a0cd8be659f7d84b216d213b0fd559a176
Andrew <github@seventh.space>
2025-03-27 15:22:36 +0000

    chore: update api port

--- react-native-app/hooks/usePopularCurrencies.ts
+++ react-native-app/hooks/usePopularCurrencies.ts
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 export function usePopularCurrencies() {
     const [data, setData] = useState<Cryptocurrency[]>([])
     const get = async () => {
-        const res = await fetch("http://127.0.0.1:8081/popular-currencies/")
+        const res = await fetch("http://127.0.0.1/popular-currencies/")
         if (res.ok) {
             setData(await res.json())
         }
